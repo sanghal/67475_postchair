9e8da522847c5838de745bf27984406c99ce92cd
Sangha <sanghal@andrew.cmu.edu>
2015-11-16 11:19:43 -0500

    frontend

--- PostChair/config/routes.rb
+++ PostChair/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :manager_associations
   resources :sensor_data
   devise_for :users
+
+  get 'home/index' => 'home#index', as: :home
+  root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
