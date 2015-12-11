904fb006e91695c33caa086b7eaacae15dd71baf
Sangha <sanghal@andrew.cmu.edu>
2015-12-11 12:36:47 -0500

    commit

--- test/factories.rb
+++ test/factories.rb
@@ -4,12 +4,12 @@ FactoryGirl.define do
     last_name "Cena"
     email "johncena@wwe.org"
     password_digest "whocares"
-    id 67475
+    id 1
   end
 
   factory :input_stream do
     user_id 0
-    set_id 67475
+    set_id 1
     position 1
     input_time DateTime.now
     measurement 0
